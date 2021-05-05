db6335021adcdb5c778d4c8c6a50739c401c6c58
HowardHughes98 <82488909+HowardHughes98@users.noreply.github.com>
2021-05-05 10:40:40 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+
+var name = prompt('Имя?');
+var age = +prompt('гоод рождения?');
+var year = 2021;
+var nowAge = ( year - age);
+var fullInfo = ( name +  '  ' + 'ваш возраст' + '  ' + nowAge +' ' + 'года');
+alert (fullInfo);
